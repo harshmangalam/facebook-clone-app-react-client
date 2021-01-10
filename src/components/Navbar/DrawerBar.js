4e4571c8712ef2f88ae30eef2cb7c97951243b06
Harsh Mangalam <mangalamharsh78606@gmail.com>
2021-01-10 20:20:37 +0530

    close btn

--- src/components/Navbar/DrawerBar.js
+++ src/components/Navbar/DrawerBar.js
@@ -3,8 +3,9 @@ import { UIContext } from '../../App'
 import { makeStyles } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
 import Toolbar from '@material-ui/core/Toolbar'
+import { Button } from '@material-ui/core'
 
-const drawerWidth = "100vw"
+const drawerWidth = '100vw'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,14 +16,14 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
     flexShrink: 0,
   },
-  drawerPaper:(darkMode)=> ({
+  drawerPaper: (darkMode) => ({
     width: drawerWidth,
-    backgroundColor:darkMode && 'rgb(36,37,38)'
+    backgroundColor: darkMode && 'rgb(36,37,38)',
   }),
 }))
 
 export default function DrawerBar({ children }) {
-  const { uiState ,uiDispatch} = useContext(UIContext)
+  const { uiState, uiDispatch } = useContext(UIContext)
   const classes = useStyles(uiState.darkMode)
 
   return (
@@ -34,10 +35,19 @@ export default function DrawerBar({ children }) {
         classes={{
           paper: classes.drawerPaper,
         }}
-        onClose={()=>uiDispatch({type:"SET_DRAWER",payload:false})}
+        onClose={() => uiDispatch({ type: 'SET_DRAWER', payload: false })}
       >
         <Toolbar />
         <div className={classes.drawerContainer}>{children}</div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() =>
+            uiDispatch({ type: 'SET_DRAWER', payload: !uiState.drawer })
+          }
+        >
+          Close
+        </Button>
       </Drawer>
     </div>
   )
